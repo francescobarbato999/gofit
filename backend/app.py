eecfd4c71ecb59638a8b25c1d2576d3afe37c7f8
gdDraz <barbato.fra99@gmail.com>
2026-08-09 23:53:43 +0200

    Add SQLAlchemy and Esericizio model

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from models import db,Esericzio
+from models import db,Esercizio
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///gofit.db"
 db.init_app(app)
--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db=SQLAlchemy()
 
-class Esericzio(db.Model):
+class Esercizio(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     nome=db.Column(db.String(100),nullable=False)
     gruppo_muscolare=db.Column(db.String(100))
